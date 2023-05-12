924790ba3e812d87a5659f15b3d1441d3504a429
majajonez <majajonez@users.noreply.github.com>
2023-05-12 10:05:00 +0200

    wyodrębnienie repository z api

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,11 +1,9 @@
-import base64
-
 from flask import (
     Blueprint, render_template, request, redirect, url_for, flash, session, g
 )
 
-from flaskr.auth import login_required, User
-from flaskr.db import get_db
+from flaskr.auth import login_required
+from flaskr.repository import *
 
 bp = Blueprint('main', __name__)
 
@@ -67,7 +65,7 @@ def event():
                 conn.commit()
                 cur.close()
                 return redirect(url_for('main.main_page'))
-            except Exception:
+            except:
                 error = "Nie udało się utworzyć wydarzenia"
         else:
             error = "Brakuje informacji"
@@ -132,112 +130,3 @@ def upload_file():
             else:
                 return redirect(url_for("main.profil"))
         return redirect(url_for("main.profil"))
-
-
-class Event:
-    def __init__(self, wydarzenie):
-        self.id = wydarzenie[0]
-        self.user_id = wydarzenie[1]
-        self.jaka_gra = wydarzenie[2]
-        self.opis = wydarzenie[3]
-        self.kiedy = wydarzenie[4]
-        self.gdzie = wydarzenie[5]
-        self.ile_miejsc = wydarzenie[6]
-        self.type = "event"
-        if wydarzenie[7]:
-            self.photo = base64.b64encode(wydarzenie[7]).decode('ascii')
-            print(self.photo)
-        self.can_join = bool(wydarzenie[8])
-        self.login = wydarzenie[9]
-
-
-class Person:
-    def __init__(self, profil):
-        self.login = profil[1]
-        self.city = profil[4]
-        self.opis = profil[5]
-        self.type = "person"
-
-
-def get_user_by_login(login):
-    conn = get_db()
-    cur = conn.cursor()
-    sql_update_query = '''SELECT * FROM logowanie_uzytkownikow WHERE login = ?'''
-    cur.execute(sql_update_query, [login])
-    uzytkownik = cur.fetchone()
-    cur.close()
-    return User(uzytkownik)
-
-
-def get_events(user_id):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute('''SELECT w.*, uw.user_id is null as can_join, lu.login FROM wydarzenia w
-                LEFT JOIN uczestnicy_wydarzen uw ON w.id = uw.event_id and uw.user_id = ?
-                LEFT JOIN logowanie_uzytkownikow lu ON w.user_id = lu.id''', [user_id])
-    wydarzenia = cur.fetchall()
-    cur.close()
-    event_list = []
-
-    for w in wydarzenia:
-        event = Event(w)
-        event_list.append(event)
-    return event_list
-
-
-def get_events_by_game(game):
-    conn = get_db()
-    cur = conn.cursor()
-    sql_update_query = '''SELECT * FROM wydarzenia WHERE jaka_gra LIKE ?'''
-    cur.execute(sql_update_query, ['%' + game + '%'])
-    wydarzenia = cur.fetchall()
-    cur.close()
-    event_list = []
-
-    for w in wydarzenia:
-        event = Event(w)
-        event_list.append(event)
-    return event_list
-
-
-def get_events_by_login(login):
-    conn = get_db()
-    cur = conn.cursor()
-    sql_update_query = '''SELECT * FROM logowanie_uzytkownikow WHERE login LIKE ?'''
-    cur.execute(sql_update_query, ['%' + login + '%'])
-    profile = cur.fetchall()
-    cur.close()
-    profil_list = []
-
-    for p in profile:
-        person = Person(p)
-        profil_list.append(person)
-    return profil_list
-
-
-def get_events_by_city(city):
-    conn = get_db()
-    cur = conn.cursor()
-    sql_update_query = '''SELECT * FROM wydarzenia WHERE city LIKE ?'''
-    cur.execute(sql_update_query, ['%' + city + '%'])
-    wydarzenia = cur.fetchall()
-    cur.close()
-    event_list = []
-
-    for w in wydarzenia:
-        event = Event(w)
-        event_list.append(event)
-    return event_list
-
-
-def get_events_by_id(id, user_id):
-    conn = get_db()
-    cur = conn.cursor()
-    sql_update_query = ('SELECT w.*, uw.user_id is null as can_join, lu.login FROM wydarzenia w'
-                        ' LEFT JOIN uczestnicy_wydarzen uw ON w.id = uw.event_id'
-                        ' LEFT JOIN logowanie_uzytkownikow lu ON w.user_id = lu.id'
-                        ' WHERE w.id = ? AND uw.user_id = ?')
-    cur.execute(sql_update_query, [id, user_id])
-    wydarzenie = cur.fetchone()
-    cur.close()
-    return Event(wydarzenie)
---
+++ flaskr/repository.py
@@ -0,0 +1,112 @@
+import base64
+
+from flaskr.auth import User
+from flaskr.db import get_db
+
+
+class Event:
+    def __init__(self, wydarzenie):
+        self.id = wydarzenie[0]
+        self.user_id = wydarzenie[1]
+        self.jaka_gra = wydarzenie[2]
+        self.opis = wydarzenie[3]
+        self.kiedy = wydarzenie[4]
+        self.gdzie = wydarzenie[5]
+        self.ile_miejsc = wydarzenie[6]
+        self.type = "event"
+        if wydarzenie[7]:
+            self.photo = base64.b64encode(wydarzenie[7]).decode('ascii')
+        self.can_join = bool(wydarzenie[8])
+        self.login = wydarzenie[9]
+
+
+class Person:
+    def __init__(self, profil):
+        self.login = profil[1]
+        self.city = profil[4]
+        self.opis = profil[5]
+        self.type = "person"
+
+
+def get_user_by_login(login):
+    conn = get_db()
+    cur = conn.cursor()
+    sql_update_query = '''SELECT * FROM logowanie_uzytkownikow WHERE login = ?'''
+    cur.execute(sql_update_query, [login])
+    uzytkownik = cur.fetchone()
+    cur.close()
+    return User(uzytkownik)
+
+
+def get_events(user_id):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute('''SELECT w.*, uw.user_id is null as can_join, lu.login FROM wydarzenia w
+                LEFT JOIN uczestnicy_wydarzen uw ON w.id = uw.event_id and uw.user_id = ?
+                LEFT JOIN logowanie_uzytkownikow lu ON w.user_id = lu.id''', [user_id])
+    wydarzenia = cur.fetchall()
+    cur.close()
+    event_list = []
+
+    for w in wydarzenia:
+        event = Event(w)
+        event_list.append(event)
+    return event_list
+
+
+def get_events_by_game(game):
+    conn = get_db()
+    cur = conn.cursor()
+    sql_update_query = '''SELECT * FROM wydarzenia WHERE jaka_gra LIKE ?'''
+    cur.execute(sql_update_query, ['%' + game + '%'])
+    wydarzenia = cur.fetchall()
+    cur.close()
+    event_list = []
+
+    for w in wydarzenia:
+        event = Event(w)
+        event_list.append(event)
+    return event_list
+
+
+def get_events_by_login(login):
+    conn = get_db()
+    cur = conn.cursor()
+    sql_update_query = '''SELECT * FROM logowanie_uzytkownikow WHERE login LIKE ?'''
+    cur.execute(sql_update_query, ['%' + login + '%'])
+    profile = cur.fetchall()
+    cur.close()
+    profil_list = []
+
+    for p in profile:
+        person = Person(p)
+        profil_list.append(person)
+    return profil_list
+
+
+def get_events_by_city(city):
+    conn = get_db()
+    cur = conn.cursor()
+    sql_update_query = '''SELECT * FROM wydarzenia WHERE city LIKE ?'''
+    cur.execute(sql_update_query, ['%' + city + '%'])
+    wydarzenia = cur.fetchall()
+    cur.close()
+    event_list = []
+
+    for w in wydarzenia:
+        event = Event(w)
+        event_list.append(event)
+    return event_list
+
+
+def get_events_by_id(id, user_id):
+    conn = get_db()
+    cur = conn.cursor()
+    sql_update_query = ('SELECT w.*, uw.user_id is null as can_join, lu.login FROM wydarzenia w'
+                        ' LEFT JOIN uczestnicy_wydarzen uw ON w.id = uw.event_id'
+                        ' LEFT JOIN logowanie_uzytkownikow lu ON w.user_id = lu.id'
+                        ' WHERE w.id = ? AND uw.user_id = ?')
+    cur.execute(sql_update_query, [id, user_id])
+    wydarzenie = cur.fetchone()
+    cur.close()
+    return Event(wydarzenie)
