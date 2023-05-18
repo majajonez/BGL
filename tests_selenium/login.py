db097f1d2e00e2916588ad75ef8e673afe622578
majajonez <majajonez@users.noreply.github.com>
2023-05-18 20:07:58 +0200

    poprawne logowanie z użyciem Selenium

--- tests_selenium/login.py
+++ tests_selenium/login.py
@@ -1,14 +1,32 @@
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 path = 'C:\\Users\\kasper\\PycharmProjects\BGL\\tests_selenium\\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=path)
 
-# driver.get('https://duckduckgo.com')
-#
-# duck = driver.find_element_by_id('logo_homepage_link')
-#
+driver.get('http://127.0.0.1:5000/auth/login')
+
+sleep(2)
+
+login_input = driver.find_element(by=By.ID, value='username')
+password_input = driver.find_element(by=By.ID, value='password')
+
+login_input.send_keys('maja')
+password_input.send_keys('123')
+
+button = driver.find_element(by=By.XPATH, value="//input[@type='submit']")
+button.click()
+
+sleep(2)
+
+driver.close()
+
+
+
+
+
 # duck.click()
 # print(duck.get_attribute('outerHTML'))
 #
