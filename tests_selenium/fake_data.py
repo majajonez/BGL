538f7a5bb65d9f78af32d8f7b2d63215c7a71731
majajonez <majajonez@users.noreply.github.com>
2023-05-19 17:21:01 +0200

    uporządkowanie kodu

--- tests_selenium/fake_data.py
+++ tests_selenium/fake_data.py
@@ -1,6 +1,18 @@
 from faker import *
 import random
 
+cities = ['Tokio', 'Delhi', 'Szanghaj', 'São Paulo', 'Meksyk', 'Kair', 'Mumbai', 'Pekin', 'Dhaka', 'Osaka',
+          'Nowy Jork', 'Karaczi', 'Calcutta', 'Istanbul', 'Buenos Aires', 'Lagos', 'Kinshasa', 'Rio de Janeiro',
+          'Manila', 'Moskwa', 'Dżakarta', 'Londyn', 'Lima', 'Bangkok', 'Nairobi', 'Los Angeles', 'Chennai',
+          'Bogota', 'Riad', 'Hongkong', 'Santiago', 'Ahmedabad', 'Bagdad', 'Singapur', 'Ankarra', 'Madryt',
+          'Berlin', 'Tajpej', 'Kairuan', 'Lakhnau', 'Johannesburg', 'Maracaibo', 'Hangzhou', 'Durban', 'Algier',
+          'Addis Abeba', 'Shijiazhuang', 'Mediolan', 'Harbin', 'Suzhou', 'Chongqing', 'Alexandria', 'Kanton',
+          'Jinan', 'Hyderbad', 'Surat', 'Pune', 'Dalian', 'Dar es Salaam', 'Shenzhen', 'Nanjing', 'Wuhan',
+          'Kuala Lumpur', 'Chengdu', 'Luanda', 'Daka', 'Kampala', 'Caracas', 'Taegu', 'Al-Kuwejt', 'Jaipur',
+          'Aleppo', 'Indore', 'Omsk', 'Kandahar', 'Lucknow', 'Baku', 'Surabaja', 'Al-Madinah', 'Dammam',
+          'Patna', 'Houston', 'Warszawa']
+
+
 class Random_user():
     def __init__(self):
         fake = Faker()
@@ -8,7 +20,4 @@ class Random_user():
         self.name = person.split(' ')[0]
         self.email = self.name + '@gmail.com'
         self.password = self.name + str(len(person))
-        cities = 'Tokio, Delhi, Szanghaj, São Paulo, Meksyk, Kair, Mumbai, Pekin, Dhaka, Osaka, Nowy Jork, Karaczi, Calcutta, Istanbul, Buenos Aires, Lagos, Kinshasa, Rio de Janeiro, Manila, Moskwa, Dżakarta, Londyn, Lima, Bangkok, Nairobi, Los Angeles, Chennai, Bogota, Riad, Hongkong, Santiago, Ahmedabad, Bagdad, Singapur, Ankarra, Madryt, Berlin, Tajpej, Kairuan, Lakhnau, Johannesburg, Maracaibo, Hangzhou, Durban, Algier, Addis Abeba, Shijiazhuang, Mediolan, Harbin, Suzhou, Chongqing, Alexandria, Kanton, Jinan, Hyderbad, Surat, Pune, Dalian, Dar es Salaam, Shenzhen, Nanjing, Wuhan, Kuala Lumpur, Chengdu, Luanda, Daka, Kampala, Caracas, Taegu, Al-Kuwejt, Jaipur, Aleppo, Indore, Omsk, Kandahar, Lucknow, Baku, Surabaja, Al-Madinah, Dammam, Patna, Houston, Warszawa'
-        cities_list = cities.split(', ')
-        self.city = random.choice(cities_list)
-
+        self.city = random.choice(cities)
